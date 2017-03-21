22bd49215cc996a895e25ee30b09a7f23796fb94
Sandy <SVern99@aol.com>
2017-03-21 17:17:18 +0000

    Set up friendships

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_the_user, only: [:show]
+  
+# Making Friends ---------------------  
+	def index
+	  case params[:people] when "friends"
+      @users = current_user.active_friends
+    when "requests"
+      @users = current_user.pending_friend_requests_from.map(&:user)
+    when "pending"
+      @users = current_user.pending_friend_requests_to.map(&:friend)
+    else
+      @users = User.where.not(id: current_user.id)
+    end
+	end
+# Making Friends ---------------------	
+
+  def show
+  end
+  
+  private
+    
+  def set_the_user
+    @user = User.find_by(username: params[:id])
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170320180248) do
+ActiveRecord::Schema.define(version: 20170321144454) do
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.string   "friend_status", default: "pending"
+    t.datetime "friended_at"
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
+  end
 
   create_table "statuses", force: :cascade do |t|
     t.string   "name"

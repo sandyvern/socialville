486f6ec68e491d71ed87aff4553c09659f9ec669
Sandy <SVern99@aol.com>
2017-03-29 01:47:17 +0000

    Added jumbotron

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,6 +1,6 @@
 class StatusesController < ApplicationController
   before_action :set_status, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index]
 
   # GET /statuses
   # GET /statuses.json
